ffd97073093aa064b2796f1891b4af741c12c489
David Gudeman <davidmgudeman@gmail.com>
2016-03-19 20:11:11 -0700

    Lesson 2 of 4 Part 1

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -68,7 +68,8 @@ angular.module('conFusion.controllers', [])
     };
   })
 
-  .controller('MenuController', ['$scope', 'menuFactory', 'baseURL', function($scope, menuFactory, baseURL) {
+  .controller('MenuController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate',
+    function($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
 
     $scope.baseURL = baseURL;
     $scope.tab = 1;
@@ -76,6 +77,7 @@ angular.module('conFusion.controllers', [])
     $scope.showDetails = false;
     $scope.showMenu = false;
     $scope.message = "Loading ...";
+
 
     menuFactory.getDishes().query(
       function(response) {
@@ -111,7 +113,13 @@ angular.module('conFusion.controllers', [])
     $scope.toggleDetails = function() {
       $scope.showDetails = !$scope.showDetails;
     };
-  }])
+      $scope.addFavorite = function (index) {
+        console.log("index is " + index);
+        favoriteFactory.addToFavorites(index);
+        $ionicListDelegate.closeOptionButtons();
+      }
+
+    }])
 
   .controller('ContactController', ['$scope', function($scope) {
 
